3294c77865feeee2635e19976f3fae3f1178e98b
nezamutdinovdmitriy <nezamutdinovdmitriy@gmail.com>
2025-11-08 21:14:53 +0500

    08.11

--- Assets/Scripts/Control/InputHandler.cs
+++ Assets/Scripts/Control/InputHandler.cs
@@ -7,14 +7,11 @@ namespace Scripts.Control
         private const string AXIS_HORIZONTAL = "Horizontal";
         private const string AXIS_VERTICAL = "Vertical";
         
-        public bool IsMoving { get; private set; }
         public Vector3 MoveVector { get; private set; }
 
         private void Update()
         {
             MoveVector = new Vector3(Input.GetAxisRaw(AXIS_HORIZONTAL), 0, Input.GetAxisRaw(AXIS_VERTICAL));
-
-            IsMoving = MoveVector != Vector3.zero;
         }
     }
 }
---
+++ Assets/Scripts/SpawnConfig.cs
@@ -0,0 +1,46 @@
+using Scripts.Entities;
+using UnityEngine;
+
+public enum IdleBehaviors
+{
+    Stay,
+    Patrol,
+    RandomWalk
+}
+
+public enum AgroBehaviors
+{
+    Chase,
+    Escape,
+    Fear
+}
+
+public class SpawnConfig : MonoBehaviour
+{
+    [SerializeField] private Enemy _enemyPrefab;
+    [SerializeField] private IdleBehaviors _idleBehaviors;
+    [SerializeField] private AgroBehaviors _agroBehaviors;
+
+    private void Awake()
+    {
+        switch (_idleBehaviors)
+        {
+            case IdleBehaviors.Stay:
+                break;
+            case IdleBehaviors.Patrol:
+                break;
+            case IdleBehaviors.RandomWalk:
+                break;
+        }
+
+        switch (_agroBehaviors)
+        {
+            case AgroBehaviors.Chase:
+                break;
+            case AgroBehaviors.Escape:
+                break;
+            case AgroBehaviors.Fear:
+                break;
+        }
+    }
+}
